f3c0b048e8a24a10ab2e7244785b96f556848b94
Sandeep Kumar <guptajii0602@gmail.com>
2026-02-26 05:56:23 +0530

    feat: Add email sending test script.

--- server/src/utils/emailTest.js
+++ server/src/utils/emailTest.js
@@ -1,6 +1,6 @@
 // generate a code to use email.js file to send email to "himanshuhaldar2000@gmail.com"
 
-import { sendVerificationEmail } from './email';
+import { sendVerificationEmail } from './email.js';
 
 sendVerificationEmail('himanshuhaldar2000@gmail.com', 'test-token').then(() => console.log('Email sent successfully')).catch((err) => console.error('Error sending email:', err));
 
